0857df7498adcb6fc280fa53740d3f1ca8f121fa
Julien RICHARD <julien.y.richard@gmail.com>
2023-09-21 17:03:52 +0200

    Ajout du set de data de départ

---
+++ script.js
@@ -0,0 +1,34 @@
+const tasks = [
+  {
+    id: 1,
+    title: "Nettoyer le sol",
+    description: "Balayage complet et serpillage. Pensez à faire les plinthes",
+    category: "home",
+    status: "todo",
+    date: "10/10/2023"
+  },
+  {
+    id: 2,
+    title: "Match de rugby",
+    description: "Match de rugby de la coupe du Monde",
+    category: "sport",
+    status: "progress",
+    date: "28/10/2023"
+  },
+  {
+    id: 4,
+    title: "To Do List Workshop",
+    description: "Preparer le support pour le live demo de to do list pour apprendre la manipulation du DOM afin de la présenter",
+    category: "code",
+    status: "done",
+    date: "27/09/2023"
+  },
+  {
+    id: 4,
+    title: "Choux kale à congeler",
+    description: "Aller chercher du choux kale, le blanchir 3 min et le repartir en sac pour congélation",
+    category: "home",
+    status: "todo",
+    date: "12/10/2023"
+  }
+]
